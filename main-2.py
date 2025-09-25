a4ad0c2ca315df13ec05662358cbf1479b3598ec
shafqatgreat <136156891+shafqatgreat@users.noreply.github.com>
2025-09-25 14:51:04 +0500

    Create main-2.py
    Request Body Implenetaion

---
+++ main-2.py
@@ -0,0 +1,41 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+VALID_TOKEN = "codingmavrick123"
+
+
+# Allow only your frontend (simple HTML file on localhost)
+origins = [
+    "http://127.0.0.1:5500",  # if you serve HTML via VSCode Live Server
+    "http://localhost:5500",
+]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,       # allow all origins 👈 frontend domains
+    allow_credentials=True,
+    allow_methods=["*"],         # allow all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],         # allow all headers (Authorization, Content-Type, etc.)
+)
+
+
+
+# Request body Model
+class AuthRequest(BaseModel):
+    token: str
+
+
+@app.post("/secure-data-body")
+def get_secure_data(auth:AuthRequest):
+    print("Token received in body:", auth.token)
+
+    if auth.token != VALID_TOKEN:
+        raise HTTPException(status_code=403,detail="Invalid or unauthorized token")
+    
+    return {"status":"Authorized","data":"Here is your secure information"}
+
+
